3dba5bdd095c961d24c053dc0b70933197e824ca
Sahil-6757 <79788148+Sahil-6757@users.noreply.github.com>
2022-01-25 15:03:04 +0530

    Added Js and some css

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-console.log("Hello World.");
-function alert()
-{
- let name = document.getElementById("exampleFormControlInput1").value;
- alert("asdf"+name);
+function allert() {
+  let name = document.getElementById("exampleFormControlInput1").value;
+  alert(name + " Congratulation your application is submitted successfully.");
+  console.log(Date());
+}
+function samba() {
+  let name = document.getElementById("exampleFormControlInput1").value;
+  let email = document.getElementById("exampleFormControlInput2").value;
+  let mesg = document.getElementById("exampleFormControlInput2").value;
+  alert(name+email+mesg);
+  
 }
